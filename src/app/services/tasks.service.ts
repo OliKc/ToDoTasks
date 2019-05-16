2ffa3fcfa1dbdd61db8e36b60f9772fc7c87a309
OliKc <oliver.kaczmar@gmail.com>
2019-05-16 18:10:45 +0200

    Update tasks.service.ts

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Task } from '../models/task';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
 
+  tasks: Array<Task> = [];
+
   constructor() { }
+
+  add(task: Task) {
+
+  }
+
+  remove(task: Task) {
+
+  }
+
+  done(task: Task) {
+
+  }
 }
