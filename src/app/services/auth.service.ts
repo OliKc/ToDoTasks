840f37da0dd1e2d82121033a6050b3e4286295c9
OliKc <oliver.kaczmar@gmail.com>
2019-05-16 17:58:34 +0200

    Update auth.service.ts
    boilerplate

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private fireAuth: AngularFireAuth) { }
+
+  user(): any {
+  }
+
+  login(): any {
+
+  }
+
+  register(): any {
+
+  }
+
+  logout(): any {
+
+  }
+
 }
