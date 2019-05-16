b1107fd2d298c32890e7aa5b4be912b5f0c1c70b
OliKc <oliver.kaczmar@gmail.com>
2019-05-16 16:14:02 +0200

    generated task components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { DoneTaskComponent } from './done-task/done-task.component';
+import { TodoTaskComponent } from './todo-task/todo-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskComponent,
+    DoneTaskComponent,
+    TodoTaskComponent
   ],
   imports: [
     BrowserModule,
