dcb12c60d13fad10bbb8968c47b04eac7a95f1a0
OliKc <oliver.kaczmar@gmail.com>
2019-05-17 21:15:10 +0200

    wip

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Credentials } from '../models/user';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  user: Credentials;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  login() {
+console.log(this.user);
+  }
+
+  signup() {
+
   }
 
 }
