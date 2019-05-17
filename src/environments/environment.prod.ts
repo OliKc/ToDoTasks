e18045529fbd4653b2f47d1c472d005b1918abd5
OliKc <oliver.kaczmar@gmail.com>
2019-05-17 14:22:26 +0200

    firebase enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDZgHfqEVo2ygoPJszLlgmTAkVXlT7qU2E",
+    authDomain: "user-login-80071.firebaseapp.com",
+    databaseURL: "https://user-login-80071.firebaseio.com",
+    projectId: "user-login-80071",
+    storageBucket: "user-login-80071.appspot.com",
+    messagingSenderId: "817349430266"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDZgHfqEVo2ygoPJszLlgmTAkVXlT7qU2E",
+    authDomain: "user-login-80071.firebaseapp.com",
+    databaseURL: "https://user-login-80071.firebaseio.com",
+    projectId: "user-login-80071",
+    storageBucket: "user-login-80071.appspot.com",
+    messagingSenderId: "817349430266"
+  }
 };
 
 /*
