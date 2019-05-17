31d6dfa04acc644917f2b7d65c8533f6410d26fc
OliKc <oliver.kaczmar@gmail.com>
2019-05-17 13:16:47 +0200

    Update tasks.service.ts

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Task } from '../models/task';
 
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
 
+  tasks$: AngularFireList<Task[]> = null;
   tasks: Array<Task> = [];
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) { }
 
   add(task: Task) {
 
