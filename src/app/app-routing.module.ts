32299e8ee02311130112207141ed0317900426ca
OliKc <oliver.kaczmar@gmail.com>
2019-05-18 23:00:53 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { SignupComponent } from './signup/signup.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/signup', pathMatch: 'full'},
+  { path: 'signup', component: SignupComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
