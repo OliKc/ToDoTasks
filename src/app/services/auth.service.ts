94a591d58b788581447e580b9184d8021566e248
OliKc <oliver.kaczmar@gmail.com>
2019-05-24 21:58:43 +0200

    Update auth.service.ts
    added uid in session storage

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,29 +1,37 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
-import { User } from 'firebase';
 import { Credentials } from '../models/user';
+import { Observable } from 'rxjs';
+import { User } from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private fireAuth: AngularFireAuth) { }
+  uid = sessionStorage.getItem('userUID');
+  readonly authState$: Observable<User | null> = this.afAuth.authState;
 
-  get user(): User | null {
-    return this.fireAuth.auth.currentUser;
-  }
+  constructor(private afAuth: AngularFireAuth) {  }
+
 
   login({email, password}: Credentials) {
-    return this.fireAuth.auth.signInWithEmailAndPassword(email, password);
+    return this.afAuth.auth.signInWithEmailAndPassword(email, password).then(
+      userCredential => {
+        sessionStorage.setItem('userUID', userCredential.user.uid);
+    });
   }
 
-  register(): any {
-
+  register({email, password}: Credentials) {
+    return this.afAuth.auth.createUserWithEmailAndPassword(email, password).then(
+      userCredential => {
+        sessionStorage.setItem('userUID', userCredential.user.uid);
+    });
   }
 
-  logout(): any {
-
+  logout() {
+    sessionStorage.removeItem('userUID');
+    return this.afAuth.auth.signOut();
   }
 
 }
