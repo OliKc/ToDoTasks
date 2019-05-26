afdb2d1d3c11a3c95530e565010e19618bf7f862
OliKc <oliver.kaczmar@gmail.com>
2019-05-26 18:56:03 +0200

    Update signup.component.ts
    error handling

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,7 @@
+import { AuthService } from '../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { Credentials } from '../models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -9,18 +11,56 @@ import { Credentials } from '../models/user';
 export class SignupComponent implements OnInit {
 
   user: Credentials;
+  email: string;
+  password: string;
 
-  constructor() { }
+  labelPosition: string;
+
+  constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit() {
+    this.labelPosition = 'login';
   }
 
   login() {
-console.log(this.user);
+    this.user = ({ email: this.email, password: this.password });
+    this.auth.login(this.user)
+    .then(() => this.router.navigate(['/dashboard']))
+    .catch(err => {
+      if (err.code === 'auth/wrong-password' || err.code === 'auth/user-not-found') {
+        alert('Wrong password or invalid user');
+
+      } else if (err.code === 'auth/invalid-email') {
+        alert(err.message);
+
+      } else if (err.code === 'auth/user-disabled') {
+        alert(err.message);
+
+      } else {
+        alert(err.message);
+      }
+    });
   }
 
   signup() {
+    this.user = ({ email: this.email, password: this.password });
+    this.auth.register(this.user)
+      .then(() => this.router.navigate(['/dashboard']))
+      .catch(err => {
+        if (err.code === 'auth/invalid-email') {
+          alert(err.message);
 
+        } else if (err.code === 'auth/email-already-in-use') {
+          alert(err.message);
+
+        } else if (err.code === 'auth/weak-password') {
+          alert(err.message);
+          
+        } else {
+          alert(err.message);
+        }
+        console.log(err);
+      });
   }
 
 }
